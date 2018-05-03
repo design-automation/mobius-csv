8fffb76790e7a6705ef08722825355c6b3b3de0f
Patrick Janssen <akiphtj@nus.edu.sg>
2018-05-03 13:18:40 +0800

    added and testes csv parse functions

---
+++ src/typescript/csv.ts
@@ -0,0 +1,40 @@
+/**
+ * Functions for working with CSV files.
+ */
+
+/**
+ *
+ */
+
+import * as pp from "papaparse";
+
+//  ===============================================================================================================
+//  String functions ==============================================================================================
+//  ===============================================================================================================
+
+/**
+ * Parse a csv file.
+ * https://www.papaparse.com/docs
+ */
+export function parse(str: string): pp.ParseResult {
+    if (str === undefined) {throw new Error("Invalid arg: str must be defined.");}
+    return pp.parse(str);
+}
+
+/**
+ * Parse a csv file, and assume that the first row is the header.
+ * https://www.papaparse.com/docs
+ */
+export function parseHasHeader(str: string): pp.ParseResult {
+    if (str === undefined) {throw new Error("Invalid arg: str must be defined.");}
+    return pp.parse(str, {header:true});
+}
+
+/**
+ * Parse a csv file, and supply a config object.
+ * https://www.papaparse.com/docs
+ */
+export function parseUseConfig(str: string, config?: pp.ParseConfig): pp.ParseResult {
+    if (str === undefined) {throw new Error("Invalid arg: str must be defined.");}
+    return pp.parse(str, config);
+}
---
+++ src/typescript/unit-tests/csv_tests.ts
@@ -0,0 +1,22 @@
+import * as tm from "../_export";
+import * as pp from "papaparse";
+import {} from "jasmine";
+
+describe("Tests for CSV Module", () => {
+    it("test_csv_parse", () => {
+        expect( test_csv_parse() ).toBe(true);
+    });
+});
+
+export function test_csv_parse(): boolean {
+    const str: string = "this,is,the,header\n5,6,7,8\na,b,c,d\ne,f,g,h"
+    const res1: pp.ParseResult = tm.csv.parse(str);
+    if (res1.data.length !== 4) {return false;}
+    if (res1.errors.length !== 0) {return false;}
+    if (res1.meta.delimiter !== ",") {return false;}
+    const res2: pp.ParseResult = tm.csv.parseUseConfig(str, {header:true});
+    if (res2.data.length !== 3) {return false;}
+    const res3: pp.ParseResult = tm.csv.parseHasHeader(str);
+    if (res3.data.length !== 3) {return false;}
+    return true;
+}
