c865306b921941283848a5d10533506465ae4798
phtj <patrick@janssen.name>
2018-05-13 14:23:27 +0800

    updated parse function

--- src/typescript/csv.ts
+++ src/typescript/csv.ts
@@ -12,17 +12,44 @@ import * as pp from "papaparse";
 
 /**
  * Parse a csv file.
- * https://www.papaparse.com/docs
+ *
+ * If the delimeter is undefined, then the delimiting character will be
+ * auto-detected from a list of common delimeters, including "," and tab.
+ *
+ * Returns a parse result object.
+ * The parse result object always contains 1) a data array, 2) an errors array, and 3) a meta object.
+ * The data array is an array of rows from teh csv.
+ * The errors array is an array of errors generated during the process of parsing the csv.
+ * The meta object contains extra information about the parse, such as delimiter used,
+ * the newline sequence, whether the process was aborted, etc.
+ *
+ * The rows in the data array will contain strings.
+ * If numeric data is required, strings will need to be converted to numbers
+ * using a string conversion function.
+ *
+ * See [https://www.papaparse.com/docs](https://www.papaparse.com/docs).
+ *
+ * @param str The text from the csv file, as a string.
+ * @param delimeter The delimeter used in the csv file.
+ * @example
+ * result = io.csv.parse(my_data)
+ * parse_data = result.data
+ * errors = result.errors
+ * meta = result.meta
+ *
+ * first_row = parsed_data[0]
+ * second_row = parsed.data[1]
  */
 export function parse(str: string, delimiter: string): pp.ParseResult {
     if (str === undefined) {throw new Error("Invalid arg: str must be defined.");}
-    if (delimiter === undefined) {delimiter = ",";}
-    return pp.parse(str, {delimiter});
+    if (delimiter === undefined) {return pp.parse(str);}
+    return pp.parse(str, {delimiter: delimiter});
 }
 
 /**
  * Parse a csv file, and supply a config object.
- * https://www.papaparse.com/docs
+ *
+ * See [https://www.papaparse.com/docs](https://www.papaparse.com/docs).
  */
 export function parseUseConfig(str: string, config?: pp.ParseConfig): pp.ParseResult {
     if (str === undefined) {throw new Error("Invalid arg: str must be defined.");}
--- src/typescript/unit-tests/csv_tests.ts
+++ src/typescript/unit-tests/csv_tests.ts
@@ -10,8 +10,9 @@ describe("Tests for CSV Module", () => {
 
 export function test_csv_parse(): boolean {
     const str: string = "this,is,the,header\n5,6,7,8\na,b,c,d\ne,f,g,h";
-    const res1: pp.ParseResult = tm.csv.parse(str, ",");
+    const res1: pp.ParseResult = tm.csv.parse(str, undefined);
     if (res1.data.length !== 4) {return false;}
+    if (res1.data[1][2] !== "7") {return false;}
     if (res1.errors.length !== 0) {return false;}
     if (res1.meta.delimiter !== ",") {return false;}
     const res2: pp.ParseResult = tm.csv.parseUseConfig(str, {header:true});
